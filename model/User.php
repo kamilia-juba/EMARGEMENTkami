4d7ad853f23ed81f2de6e49dafac6f83f167f38a
Bersipont Amine <bersipont.iphs@gmail.com>
2022-12-09 19:38:00 +0100

    build(UC_Model): création des fonctions permettant de récuperer un objet User ou un tableau de User depuis la base de donnée selon un certain critère

--- model/User.php
+++ model/User.php
@@ -8,4 +8,34 @@ class User extends Model {
     public function __construct(public string $mail, public string $hashed_password, public string $full_name, public string $role, public ?string $iban = null){
         
     }
+
+    public static function get_user_by_id(int $id) : User|false {
+        $query = self::execute("SELECT * FROM Users where id = :id", ["id"=>$id]);
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) {
+            return false;
+        } else {
+            return new User($data["mail"], $data["hashed_password"], $data["full_name"], $data["role"], $data["iban"]);
+        }
+    }
+
+    public static function get_user_by_mail(String $mail) : User|false {
+        $query = self::execute("SELECT * FROM Users where id = :id", ["mail"=>$mail]);
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) {
+            return false;
+        } else {
+            return new User($data["mail"], $data["hashed_password"], $data["full_name"], $data["role"], $data["iban"]);
+        }
+    }
+
+    public static function get_users() : array {
+        $query = self::execute("SELECT * FROM Users", []);
+        $data = $query->fetchAll();
+        $results = [];
+        foreach ($data as $row) {
+            $results[] = new User($row["mail"], $row["hashed_password"], $row["full_name"], $row["role"], $row["iban"]);
+        }
+        return $results;
+    }
 }
