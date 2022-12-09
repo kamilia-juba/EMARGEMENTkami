318bf90ff55766b4f69790fee07f1295b7d59bc3
Mustafa Aydogan <aydomus3569@hotmail.com>
2022-12-09 19:43:35 +0100

    build(UC_Model): création de la méthode permettant de récupérer les tricounts par l'utilisateur connecté

--- model/Tricount.php
+++ model/Tricount.php
@@ -7,6 +7,17 @@ class Tricount extends Model{
     public function __construct(public ?int $id = null, public string $title, public ?string $description = null, public string $created_at, public int $creator ){
         
     }
+
+    public static function get_user_tricounts(User $user) : array {
+        $query = self::execute("select * from tricounts where creator = :userId", ["userId"=>$user->$id]);
+        $data = $query->fetchAll();
+        $results = [];
+        foreach ($data as $row){
+            $results[] = new Tricount($row["id"], $row["title"], $row["description"], $row["created_at"], $row["creator"] );
+        }
+
+        return $results;
+    }
 }
 
 ?>
