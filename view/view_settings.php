f6c93446bffafa3be1f11bbd8ef49acb2f4bc85c
Bersipont Amine <bersipont.iphs@gmail.com>
2022-12-14 11:22:31 +0100

    feat(UC_Settings): création de la vue settings

---
+++ view/view_settings.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Settings</title>
+        <base href="<?= $web_root ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="title"> Hey <?= $user->full_name ?>!</div>
+        <br><br>
+        <div class="text"> I know your email address is <?= $user->mail?>. </div>
+        <br><br>
+        <button onclick="location.href = 'main/logout';" id="myButton" class="float-left submit-button" >Log out</button>
+        <br><br>
+        <button onclick="location.href = '';" id="myButton" class="float-left submit-button" >Edit profile</button>
+        <br><br>
+        <button onclick="location.href = '';" id="myButton" class="float-left submit-button" >Edit profile</button>
+
+    </body>
+</html>
