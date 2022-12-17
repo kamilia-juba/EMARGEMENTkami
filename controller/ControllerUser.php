5b036013692c87a2865be16275ff8bb0b0571d24
Bersipont Amine <bersipont.iphs@gmail.com>
2022-12-17 17:28:25 +0100

    feat(UC_EditProfile): ajout de la méthode edit_profile

--- controller/ControllerUser.php
+++ controller/ControllerUser.php
@@ -16,6 +16,39 @@ class ControllerUser extends MyController {
             (new View("settings"))->show(["user" => $user]);
         }
     }
+
+        //gestion de l'édition du profil
+    public function edit_profile() : void {
+        if (!$this->user_logged()) {
+            $this->redirect("Main");
+        }
+        else{
+            $user = $this->get_user_or_redirect();
+            $errors = [];
+            $success = "";
+
+
+            if (isset($_POST['full_name']) || isset($_POST['IBAN']) ) {
+                $full_name = $_POST['full_name'];
+                $iban = $_POST['iban'];
+                $user->full_name = $full_name;
+                $user->iban = $iban;
+                $user->persist();
+            }
+            
+            // si on est en POST et sans erreurs, on redirige avec un paramètre 'ok'
+            if (count($_POST) > 0 && count($errors) == 0){
+            $this->redirect("user", "edit_profile", "ok");
+            }
+
+            // si param 'ok' dans l'url, on affiche le message de succès
+            if (isset($_GET['param1']) && $_GET['param1'] === "ok"){
+            $success = "Your profile has been successfully updated.";
+            }
+
+            (new View("edit_profile"))->show(["iban" => $user->iban, "full_name" => $user->full_name , "errors" => $errors, "success" => $success]);
+        }
+    }
 }
 
 
