dd6c57a1acf3986267fde01cab29d29abd1cd9ee
Saada Yacine CHAKER <yacinechakler1996@gmail.com>
2022-12-22 10:21:10 +0100

    fix(UC_Addtricount) : correction des erreurs )

--- view/view_addtricount.php
+++ view/view_addtricount.php
@@ -7,24 +7,35 @@
     </head>
     <body>
         <div class="titlebar">
-            <input type="submit" style"float: left" name="buttonCancel" value ="Cancel">
+        <a href = "Tricount/yourTricounts/">  <button type="button" name="buttonAdd">Cancel</button></a>
+
+      
+        <!--    <a href = "Tricount/yourTricounts/" onclick input type="submit" style"float: left" name="buttonCancel" value ="Cancel">-->
+
              Tricount->add
-            <input type="submit" style"float: right" name="buttonAdd" value="save">
+             
         </div>
         
 
         <div class="main">
             <br><br>
-            <form id="addTricountForm" action="Tricount/addTricount" method="post">
-                <table>
-                    <tr>
-                        <td>Title:</td>
-                        <td><input id="mail" name="mail" type="text" size="25" value="<?= $title ?>"></td>
-                    </tr>
-                    <tr>
-                        <td>Description(optional):</td>
-                        <td><input id="full_name" name="full_name" type="text" size="16" value="<?= $description ?>"></td>
-                    </tr>
+            <form action="Tricount/addtricount" method="post">
+            <label for="title">Titre :</label><br>
+            <input type="text" id="title" name="title"><br>
+            <label for="description">Description :</label><br>
+            <input type="text" id="description" name="description"><br><br>
+             <button type="submit" name="buttonAdd">add</button></a>
+            </form> 
+            <?php if (count($errors) != 0): ?>
+                <div class='errors'>
+                    <p>Please correct the following error(s) :</p>
+                    <ul>
+                        <?php foreach ($errors as $error): ?>
+                            <li><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
                   
         </div>        
     </body>
