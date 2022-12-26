556fb6ac02dc0b5388d0162b41038ed8b84a5c07
Mustafa Aydogan <aydomus3569@hotmail.com>
2022-12-26 15:25:21 +0100

    feat(UC_ViewBalance): implémentation de la titlebar et ajout du bouton back fonctionnel

--- view/view_balance.php
+++ view/view_balance.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Balance</title>
+    <base href="<?= $web_root ?>"/>
 </head>
 <body>
+    <section id="titlebar">
+    <a href="Tricount/showTricount/<?= $tricount->id?>"><button type ="button" name="buttonBack">Back</button></a>
+        <?=$tricount->title?> &#8594 Balance
+    </section>
     <table>
         <?php
             for($i=0;$i<sizeof($tricount->get_totals());++$i){
