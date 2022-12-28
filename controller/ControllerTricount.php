fd2bebd3b0aa661b955cbb4b07c11ed6c9530e0f
Mustafa Aydogan <aydomus3569@hotmail.com>
2022-12-28 14:04:06 +0100

    fix(ControllerTricount): suppression de quelques conditions inutiles dans le controller tricount

--- controller/ControllerTricount.php
+++ controller/ControllerTricount.php
@@ -12,26 +12,18 @@ class ControllerTricount extends MyController{
     }
 
     public function yourTricounts(): void {
-        if ($this->user_logged()) {
-            $user = $this->get_user_or_redirect();
-            $tricounts = $user->get_user_tricounts();
-            (new View("listTricounts"))->show(["tricounts" => $tricounts]);
-        } else {
-            $this->redirect("Main");
-        }
+        $user = $this->get_user_or_redirect();
+        $tricounts = $user->get_user_tricounts();
+        (new View("listTricounts"))->show(["tricounts" => $tricounts]);
     }
 
     public function showTricount(): void{
-        if($this->user_logged()){
-            $user = $this->get_user_or_redirect();
-            if (isset($_GET["param1"]) && $_GET["param1"] !== "") {
-                $tricount = Tricount::getTricountById($_GET["param1"], $user->mail);
-                $operations = Operation::get_operations_by_tricountid($tricount->id);
-            }
-            (new View("tricount"))->show(["tricount" => $tricount, "operations" => $operations,"user"=>$user]);
-        }else{
-            $this->redirect("Main");
+        $user = $this->get_user_or_redirect();
+        if (isset($_GET["param1"]) && $_GET["param1"] !== "") {
+            $tricount = Tricount::getTricountById($_GET["param1"], $user->mail);
+            $operations = Operation::get_operations_by_tricountid($tricount->id);
         }
+        (new View("tricount"))->show(["tricount" => $tricount, "operations" => $operations,"user"=>$user]);
     }
 
     public function showBalance(): void{
