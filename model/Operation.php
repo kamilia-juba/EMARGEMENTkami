77148b0d30c2f73a5aee5d2f12f2ea65ae2600e2
Bersipont Amine <bersipont.iphs@gmail.com>
2022-12-28 22:32:50 +0100

    feat(UC_AddOperation): ajout d'une méthode persist afin de sauver l'operation en base de donnée

--- model/Operation.php
+++ model/Operation.php
@@ -6,8 +6,13 @@ require_once "model/Tricount.php";
 
 class Operation extends Model {
 
-    public function __construct(public string $title, public int $tricount,public float $amount,
-                                  public int $initiator, public ?string $created_at=null,public ?string $operation_date=null, public ?int $id=null){
+    public function __construct(public string $title, 
+                                public int $tricount,
+                                public float $amount,
+                                public int $initiator,
+                                public ?string $created_at=null,
+                                public ?string $operation_date=null,
+                                public ?int $id=null){
       
     }
 
@@ -31,6 +36,13 @@ class Operation extends Model {
         return $errors;
     }
 
+    public static function get_operation_by_id(int $id) : Operation{
+        $query = self::execute("SELECT * FROM operations WHERE tricount = :tricountId");
+        $data = $query->fetch();
+        return new Operation($data["mail"],$data["hashed_password"],$data["full_name"],$data["role"],$data["iban"], $data["id"]);
+
+    }
+
     public static function get_operations_by_tricountid(int $tricountId) : array{
 
         $query = self::execute("SELECT * FROM operations WHERE tricount = :tricountId order by created_at DESC", ["tricountId" => $tricountId]);
@@ -47,6 +59,18 @@ class Operation extends Model {
         $query = self::execute("SELECT * from Users where id = (SELECT initiator FROM operations WHERE id=:id)",["id"=>$this->id]);
         $data = $query->fetch();
         return new User($data["mail"],$data["hashed_password"],$data["full_name"],$data["role"],$data["iban"], $data["id"]);
+    }
+
+    public function persist() : Operation {
+        
+        self::execute("INSERT INTO operations(title,tricount,amount,operation_date,initiator,created_at) VALUES(:title,:tricount,:amount,:operation_date,:initiator,:created_at)", 
+                        [ "title"=>$this->title,
+                        "tricount"=>$this->tricount,
+                        "amount"=>$this->amount,
+                        "operation_date"=>$this->operation_date,
+                        "initiator"=>$this->initiator,
+                        "created_at"=>$this->created_at]);
+        return $this;
     }
 }
 
