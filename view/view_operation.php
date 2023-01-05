2b911663d3539d31d7979653e5d87a6021ee377e
Mustafa <aydomus3569@hotmail.com>
2023-01-05 13:40:22 +0100

    fix(UC_ViewOperation): mini changement au niveau du titre qui affichait 'Document' au lieu du titre de l'opération

--- view/view_operation.php
+++ view/view_operation.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <base href="<?= $web_root ?>"/>
-        <title>Document</title>
+        <title><?=$operation->title?></title>
     </head>
     <body>
         <section id="titlebar">
