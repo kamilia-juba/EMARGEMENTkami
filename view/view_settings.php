ff436a81cf14d809613ab6ed2a12bc5b67533bca
Bersipont Amine <bersipont.iphs@gmail.com>
2023-01-27 21:47:42 +0100

    feat(UC_Settings): ajout de la première partie du css

--- view/view_settings.php
+++ view/view_settings.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <a href = "tricount/yourTricounts"> Back </a>
+    <div class="pt-3 ps-3 pe-3 pb-3 text-secondary d-flex justify-content-between" style="background-color: #E3F3FD "> 
+        <a href= "" class= "btn btn-outline-danger" name = "buttonBack">Back</a> Settings</div>
         <meta charset="UTF-8">
         <title>Settings</title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
-        <div class="title"> Hey <?= $user->full_name ?>!</div>
-        <br><br>
-        <div class="text"> I know your email address is <?= $user->mail?>. </div>
-        <br><br>
-        <a href = "user/edit_profile"> Edit Profile </a>
-        <br><br>
-        <a href = "user/change_password"> Change Password </a>
-        <br><br>
-        <a href = "user/logout"> Logout </a>
-        <br><br>
+        <div class="pt-3 ps-3 pe-3 pb-3 "> 
+            <p>Hey <?= $user->full_name ?>!</p>
+            <p style="display:inline">  I know your email address is <p style="display:inline" class="text-danger"><?= $user->mail?></p></p>
+            <p>What can I do you for you?</p>
+        </div>
+
     </body>
+    <footer>    
+        <div class="text-center">
+        <a href = "user/edit_profile"><button type="button" class="btn btn-outline-primary col-11">Edit Profile</button></a>
+        <p></p>
+        <a href = "user/change_password"><button type="button" class="btn btn-outline-primary col-11">Change Password</button></a>
+        <p></p>
+        <a href = "user/logout"><button type="button" class="btn btn-danger col-11">Logout</button></a>
+        <br></br>
+        </div>
+    </footer>
 </html>
