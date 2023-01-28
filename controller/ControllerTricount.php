12f306246854a329ed79e915541005459619daaa
Saada Yacine CHAKER <yacinechakler1996@gmail.com>
2023-01-28 20:25:18 +0100

    feat(UC_viewTemplates) : creation de la methode showTemplates

--- controller/ControllerTricount.php
+++ controller/ControllerTricount.php
@@ -85,5 +85,20 @@ class ControllerTricount extends MyController{
             $this->redirect("Main");
         }
     }
+
+    public function showTemplates(): void{
+        $user=$this->get_user_or_redirect();
+        if(isset($_GET["param1"]) && $_GET["param1"] !== "" && $user->isSubscribedToTricount($_GET["param1"])){
+            $tricount = Tricount::getTricountById($_GET["param1"]);
+            $templates = $tricount->get_repartition_templates();
+            $templates_items = [];
+            foreach($templates as $template){
+                $templates_items[] = [$template->get_repartition_template_users(),$template];
+            }
+            (new View("templates"))->show(["tricount" => $tricount, "templates_items" => $templates_items]);
+        }else{
+            $this->redirect("Main");
+        }
+    }
 }
 ?>
