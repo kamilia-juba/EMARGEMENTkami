2fb57e7f59ef82f68e524e82acef72eec7447592
Mustafa <aydomus3569@hotmail.com>
2023-02-04 18:46:17 +0100

    fix(UC_addTemplate): ajout d'un container pour tous les éléments du body (sauf le header)

--- view/view_add_template.php
+++ view/view_add_template.php
@@ -11,39 +11,41 @@
  
 
     <body>
+        
         <div class="pt-2 ps-3 pe-3 pb-2 text-secondary d-flex justify-content-between" style="background-color: #E3F3FD">
             <a href="Tricount/showTemplates/<?=$tricount->id?>" class="btn btn-outline-danger" name="buttonCancel">Cancel</a>
             <?=$tricount->title?> &#8594 New template
             <form id="addtemplateForm" action="Tricount/addTemplate/<?=$tricount->id?>" method="post">
             <input type="submit"class="btn btn-primary" value="Save" form="addtemplateForm"></a><br>
         </div> 
-        
-        Title : 
-        <input class="form-control mb-2" id="title" name="title" type="text" placeholder="Title">
-         Template items :
-            <?php foreach($participants as $participant) { ?>
-                <div class="input-group mb-2 mt-2">
-                    <span class="form-control" style="background-color: #E9ECEF">
-                    
-                    <input type="checkbox"name="checkboxParticipants[]"value="<?=$participant->id?>"checked>
+        <div class="container">
+            Title : 
+            <input class="form-control mb-2" id="title" name="title" type="text" placeholder="Title">
+            Template items :
+                <?php foreach($participants as $participant) { ?>
+                    <div class="input-group mb-2 mt-2">
+                        <span class="form-control" style="background-color: #E9ECEF">
                         
-                    
-                      
-                    </span>  
-                    <span class="input-group-text w-75" style="background-color: #E9ECEF"><?=$participant->full_name?></span>
-                     <input class="form-control" type="number" min="0" name="weight[]" value="1">
-                </div>
-            <?php } ?>
-        </table>
-    </form>
-    <?php if (count($errors) != 0): ?>
-        <div class="text-danger">
-                    <ul>
-                        <?php foreach ($errors as $error): ?>
-                            <li><?= $error ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-    <?php endif; ?> 
+                        <input type="checkbox"name="checkboxParticipants[]"value="<?=$participant->id?>"checked>
+                            
+                        
+                        
+                        </span>  
+                        <span class="input-group-text w-75" style="background-color: #E9ECEF"><?=$participant->full_name?></span>
+                        <input class="form-control" type="number" min="0" name="weight[]" value="1">
+                    </div>
+                <?php } ?>
+            </table>
+        </form>
+        <?php if (count($errors) != 0): ?>
+            <div class="text-danger">
+                        <ul>
+                            <?php foreach ($errors as $error): ?>
+                                <li><?= $error ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+        <?php endif; ?> 
+    </div>
 </body>
 </html>
