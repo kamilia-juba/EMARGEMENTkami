1c3c46c7c69c44a2022091165d102c5ba3af17f9
Saada Yacine CHAKER <yacinechakler1996@gmail.com>
2023-02-04 18:32:16 +0100

    feat(UC_addTemplates) : ajout du css a la vue addTemplate

--- view/view_add_template.php
+++ view/view_add_template.php
@@ -1,33 +1,43 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="<?= $web_root ?>"/>
-    <title>Add Template</title>
-</head>
-<body>
-    <a href="Tricount/showTemplates/<?=$tricount->id?>"><button type="button" name="buttonCancel">Cancel</button></a>
-    <?=$tricount->title?> &#8594 New template
-    <input type="submit" name = "buttonSave" value="Save" form="addtemplateForm"></a><br>
-    <form id="addtemplateForm" action="Tricount/addTemplate/<?=$tricount->id?>" method="post">
-        Title:<br>    
-        <input type="text" name="title">
-        Template items:<br>
-        <table>
+    <head>
+        <meta charset="UTF-8">
+        <title>Add Template</title>
+        <base href="<?= $web_root ?>"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+ 
+
+    <body>
+        <div class="pt-2 ps-3 pe-3 pb-2 text-secondary d-flex justify-content-between" style="background-color: #E3F3FD">
+            <a href="Tricount/showTemplates/<?=$tricount->id?>" class="btn btn-outline-danger" name="buttonCancel">Cancel</a>
+            <?=$tricount->title?> &#8594 New template
+            <form id="addtemplateForm" action="Tricount/addTemplate/<?=$tricount->id?>" method="post">
+            <input type="submit"class="btn btn-primary" value="Save" form="addtemplateForm"></a><br>
+        </div> 
+        
+        Title : 
+        <input class="form-control mb-2" id="title" name="title" type="text" placeholder="Title">
+         Template items :
             <?php foreach($participants as $participant) { ?>
-                    <tr>
-                        <td><input type="checkbox" name="checkboxParticipants[]" value="<?=$participant->id?>" checked></td>
-                        <td><?=$participant->full_name?></td>
-                        <td><input type="number" name="weight[]" value="1" min="0"></td>
-                    </tr>
+                <div class="input-group mb-2 mt-2">
+                    <span class="form-control" style="background-color: #E9ECEF">
+                    
+                    <input type="checkbox"name="checkboxParticipants[]"value="<?=$participant->id?>"checked>
+                        
+                    
+                      
+                    </span>  
+                    <span class="input-group-text w-75" style="background-color: #E9ECEF"><?=$participant->full_name?></span>
+                     <input class="form-control" type="number" min="0" name="weight[]" value="1">
+                </div>
             <?php } ?>
         </table>
     </form>
     <?php if (count($errors) != 0): ?>
-                <div class='errors'>
-                    <p>Please correct the following error(s) :</p>
+        <div class="text-danger">
                     <ul>
                         <?php foreach ($errors as $error): ?>
                             <li><?= $error ?></li>
