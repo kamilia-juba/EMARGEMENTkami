25008d582a6bc101e93038a70ed2842f9a0cb87a
Mustafa <aydomus3569@hotmail.com>
2023-03-08 15:34:38 +0100

    fix(UC_Login, UC_Signup): correction apportée dans le controller: un user connecté pouvait accéder à la page de Login et Signup via l'url alors qu'il n'était pas censé pouvoir

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -17,51 +17,60 @@ class ControllerMain extends MyController {
 
     //gestion de la connexion d'un utilisateur
     public function login() : void {
-        $password = '';
-        $mail = "";
-        $errors = [];
-        if (isset($_POST['mail']) && isset($_POST['password'])) { 
-            $mail = Tools::sanitize($_POST['mail']);
-            $password = Tools::sanitize($_POST['password']);
-
-            $errors = User::validate_login($mail, $password);
-            if (empty($errors)) {
-                $this->log_user(User::get_user_by_mail($mail));
+        if($this->user_logged() || (isset($_GET["param1"]) && $_GET["param1"] != "")){
+            $this->redirect("Tricount","yourTricounts");
+        }else {
+            $password = '';
+            $mail = "";
+            $errors = [];
+            if (isset($_POST['mail']) && isset($_POST['password'])) { 
+                $mail = Tools::sanitize($_POST['mail']);
+                $password = Tools::sanitize($_POST['password']);
+    
+                $errors = User::validate_login($mail, $password);
+                if (empty($errors)) {
+                    $this->log_user(User::get_user_by_mail($mail));
+                }
             }
+            (new View("login"))->show(["mail" => $mail, "password" => $password, "errors" => $errors]);
         }
-        (new View("login"))->show(["mail" => $mail, "password" => $password, "errors" => $errors]);
     }
     //gestion de l'inscription d'un nouvel utilisateur
     public function signup() : void {
-        $mail = '';
-        $full_name='';
-        $IBAN='';
-        $password = '';
-        $password_confirm = '';
-        $errors = [];
-
-        if (isset($_POST['mail']) && isset($_POST['full_name']) && 
-            isset($_POST['password']) && isset($_POST['password_confirm'])) {
-           
-            $mail = Tools::sanitize($_POST['mail']);
-            $full_name = Tools::sanitize($_POST['full_name']);
-            $IBAN = Tools::sanitize($_POST['IBAN']);
-            $password = Tools::sanitize($_POST['password']);
-            $password_confirm = Tools::sanitize($_POST['password_confirm']);
-            
-
-            $errors =array_merge($errors, User::getSignupErrors($mail,$full_name,$IBAN,$password,$password_confirm));
-            
-            if (count($errors) == 0) { 
-                $user = new User($mail ,Tools::my_hash($password), $full_name , "user" ,$IBAN );
-                $user->persist(); //sauve l'utilisateur
-                $this->log_user($user);
-            }
-        }
+        if($this->user_logged() || (isset($_GET["param1"]) && $_GET["param1"] != "")){
+            $this->redirect("Tricount","yourTricounts");
+        }else{
+            $mail = '';
+            $full_name='';
+            $IBAN='';
+            $password = '';
+            $password_confirm = '';
+            $errors = [];
     
-
-        (new View("signup"))->show(["mail" => $mail, 'full_name'=> $full_name,'IBAN'=> $IBAN, "password" => $password, 
-                                         "password_confirm" => $password_confirm, "errors" => $errors]);
+            if (isset($_POST['mail']) && isset($_POST['full_name']) && 
+                isset($_POST['password']) && isset($_POST['password_confirm'])) {
+               
+                $mail = Tools::sanitize($_POST['mail']);
+                $full_name = Tools::sanitize($_POST['full_name']);
+                $IBAN = Tools::sanitize($_POST['IBAN']);
+                $password = Tools::sanitize($_POST['password']);
+                $password_confirm = Tools::sanitize($_POST['password_confirm']);
+                
+    
+                $errors =array_merge($errors, User::getSignupErrors($mail,$full_name,$IBAN,$password,$password_confirm));
+                
+                if (count($errors) == 0) { 
+                    $user = new User($mail ,Tools::my_hash($password), $full_name , "user" ,$IBAN );
+                    $user->persist(); //sauve l'utilisateur
+                    $this->log_user($user);
+                }
+            }
+        
+    
+            (new View("signup"))->show(["mail" => $mail, 'full_name'=> $full_name,'IBAN'=> $IBAN, "password" => $password, 
+                                             "password_confirm" => $password_confirm, "errors" => $errors]);
+        }
+        
     }
 
 }
