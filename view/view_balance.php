2974ae735bd9090935d6a1668dc91774a3edf4c0
Amine Bersipont <bersipont.iphs@gmail.com>
2023-05-22 21:29:44 +0200

    feat(UC_ViewBalance): ajout de l'affichage des montants à l'aide d'un plugin annexe a chart.js

--- view/view_balance.php
+++ view/view_balance.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="lib/jquery-3.6.3.min.js" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0/dist/chartjs-plugin-datalabels.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script>
 
@@ -91,6 +91,8 @@
             }]
         };
 
+
+        
         var options = {
             indexAxis: 'y',
             scales: {
@@ -108,9 +110,34 @@
                 },
             },
             plugins: {
-                legend: {
-                    display: false
+            legend: {
+                display: false
+            },
+            datalabels: {
+                anchor: 'center',
+                align: function(context) {
+                            var value = parseInt(context.dataset.data[context.dataIndex]);
+                            if(value>=0){
+                                return "left";
+                            } else{
+                                return "right";
+                            };
                 },
+                offset: function(context) {
+                            var value = context.dataset.data[context.dataIndex];
+                            if(value>=0){
+                                return max*value/1000;
+                            } else{
+                                return -max*value/1000-25;
+                            };
+                },
+                formatter: function(value, context) {
+                    return value + ' €';
+                },
+                font: {
+                    weight: 'bold'
+                },                
+            },
                 tooltip: {
                     callbacks: {
                         label: function(context) {
@@ -128,6 +155,7 @@
             type: 'bar',
             data: data,
             options: options,
+            plugins: [ChartDataLabels],
         });
     </script>
 
