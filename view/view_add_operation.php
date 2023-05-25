108b40ca5831f8724191120a441bd74c2404dafc
Mustafa Aydogan <aydomus3569@hotmail.com>
2023-05-25 20:30:40 +0200

    fix(UC_editOperation,UC_addOperation): l'encodage du new_template_name était perdue après erreur PHP dans editOperation + idem pour la checkbox saveTemplate pour addOperation

--- view/view_add_operation.php
+++ view/view_add_operation.php
@@ -593,7 +593,7 @@
             </div>
             Add a new repartition template
             <div class="input-group mb-2 pt-2 pb-2">
-                <span class="form-control" style="background-color: #E9ECEF"><input type="checkbox" id="saveTemplateCheck" name="saveTemplateCheck"></span>
+                <span class="form-control" style="background-color: #E9ECEF"><input type="checkbox" id="saveTemplateCheck" name="saveTemplateCheck" <?= $save_template_checked?>></span>
                 <span class="input-group-text" style="background-color: #E9ECEF">Save this template</span>
                 <input class="form-control w-50" id="newTemplateName" name="newTemplateName" value="<?=$save_template_name?>">
             </div>
